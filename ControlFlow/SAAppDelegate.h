eb4b4b28a5ee995964ee783f4e5ff5740c87dbd9
Sachin Ahuja <Sachin@sachin-ahujas-macbook-pro.local>
2014-03-03 14:25:15 -0600

    Initial Commit

---
+++ ControlFlow/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  ControlFlow
+//
+//  Created by Sachin Ahuja on 3/3/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
