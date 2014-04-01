9f32780465cca805ea371ebff910b5e91adefe3a
Your Name <you@example.com>
2014-03-31 21:13:49 -0500

    first commit

--- ControlFlow/SAViewController.m
+++ ControlFlow/SAViewController.m
@@ -18,6 +18,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    int truckSpeed = 45;
+    int lamboSpeed = 120;
+    int carSpeed = 80;
+    int mySpeed = carSpeed;
+    
+    // < > <= >= != == || &&
+    
+    if (mySpeed < 70) {
+        NSLog(@"Keep Crusing");
+    }
+    else if (mySpeed > 55 && mySpeed < 90)
+        NSLog(@"Crusing down highway 66");
+    else {
+        NSLog(@"Slow Down");
+    }
+    BOOL isTelevisionOn = NO;
+    
+    if (isTelevisionOn != YES){
+        NSLog(@"We should take a break and do some coding");
+    }
+    else {
+        NSLog(@"great job keep up the hard work");
+    }
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
